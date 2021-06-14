eca32b8068b5036c3b1a4f583b65faa2a45132af
Jacopo <beschi.jacopo@gmail.com>
2021-06-14 12:25:16 +0200

    Fix multiple setup! calls to multi_db
    When calling setup! multiple times the ObserverExtension led to
    an infinite loop.
    Using prepend avoids this issue.

--- lib/multi_db/observer_extensions.rb
+++ lib/multi_db/observer_extensions.rb
@@ -1,17 +1,13 @@
 module MultiDb
   module ObserverExtensions
-    def self.included(base)
-      base.alias_method_chain :update, :masterdb
-    end
-
     # Send observed_method(object) if the method exists.
-    def update_with_masterdb(observed_method, object) #:nodoc:
+    def update(observed_method, object, *extra_args, &block) #:nodoc:
       if object.connection.respond_to?(:with_master)
         object.connection.with_master do
-          update_without_masterdb(observed_method, object)
+          super
         end
       else
-        update_without_masterdb(observed_method, object)
+        super
       end
     end
   end
